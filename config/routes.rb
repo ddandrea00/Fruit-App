d22ffb588791cfa79cc68cbc909837d2d7c834eb
Drew Dandrea <d.dandrea00@gmail.com>
2017-10-23 15:53:48 -0400

    still testing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root to: "fruits#index"
 
-  get '/sign_in' => 'devise/sessions#new'
-  
   devise_for :users, :controllers => { registrations: 'registrations' }, :path_prefix => 'd'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
