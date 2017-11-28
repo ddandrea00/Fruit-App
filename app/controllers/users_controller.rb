a6b6c0bef6f4ba949bc1a6156ab36fadb44f7c3a
Drew Dandrea <d.dandrea00@gmail.com>
2017-11-28 13:59:37 -0500

    getting paperclip and profile picture to upload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
   
   def show
-  user = User.find(params[:id])
+  @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
   end
 
   def user_params
-   params.require(:user).permit(:picture)
-  end
+      params.require(:user).permit(:image)
+      end
 end
